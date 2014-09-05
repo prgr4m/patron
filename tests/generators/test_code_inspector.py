954eb55a7f003e3ed5ecc7d216a8578de0df9d1c
prgr4m <ping_me@johnboisselle.com>
2014-09-05 19:56:44 -0400

    adding test case for CodeInspector

---
+++ tests/generators/test_code_inspector.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import unittest
+from stencil.generators import CodeInspector
+
+
+class TestCodeInspector(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
--- tests/stencil_test.py
+++ tests/stencil_test.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import os.path as path
+import sys
 import unittest
+
+current_location = path.dirname(path.abspath(__file__))
+lib_dir = path.join(path.abspath(path.join(current_location, os.pardir)), 'lib')
+sys.path.insert(0, lib_dir)
 
 if __name__ == '__main__':
     tests = unittest.TestLoader().discover('generators')
