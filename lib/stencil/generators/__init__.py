332923af4f30f0736de1ac89a0c1a1a480d4ff2c
prgr4m <ping_me@johnboisselle.com>
2014-09-05 22:47:46 -0400

    added vimrc file to the project

--- lib/stencil/generators/__init__.py
+++ lib/stencil/generators/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
+
+
 class CodeInspector:
     "Imports a given module and inspects for code generation collisions"
-    pass
+    @staticmethod
+    def inspect(module, attribute):
+        if not os.path.exists(module):
+            raise OSError('FileNotFoundError')  # FileNotFoundError in python3.4
 
 __all__ = [CodeInspector]
--- tests/generators/test_code_inspector.py
+++ tests/generators/test_code_inspector.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 import unittest
+import tempfile
+import textwrap
 from stencil.generators import CodeInspector
 
 
 class TestCodeInspector(unittest.TestCase):
     def setUp(self):
-        pass
+        self.dummy_template = textwrap.fill("""
+        import os
+        from flask import abort, redirect
+
+        # you do realize this isn't supposed to be real...
+
+        @blueprint.route('/', methods=['GET'])
+        def index():
+            return render_template('index.html')
+        """)
 
     def tearDown(self):
         pass
+
+    def test_checks_if_module_exists(self):
+        with tempfile.TemporaryFile() as tmp_file:
+            tmp_file.write(self.dummy_template)
+            print(tmp_file)
+            self.assertEqual(CodeInspector)
