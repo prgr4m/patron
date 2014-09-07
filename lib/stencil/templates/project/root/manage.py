9b5ff38cd6a18eba2c7da798b18c7b73088b958d
prgr4m <ping_me@johnboisselle.com>
2014-09-07 19:34:00 -0400

    removed livereload from server and is moving to front-end

--- lib/stencil/templates/project/root/manage.py
+++ lib/stencil/templates/project/root/manage.py
@@ -34,33 +34,6 @@ def make_context():
 
 manager.add_command("shell", Shell(make_context=make_context))
 
-@manager.option('-p', '--port', dest='port', default=5000)
-def liveserver(port):
-    "Run LiveReload Server with Flask"
-    static_dir = path.join(os.getcwd(), "$project_name", "static")
-    sass_globs = ['*.sass', 'base/*.sass', 'modules/*.sass']
-    sass_conf = {
-        'sass': path.join(static_dir,'sass'),
-        'css': path.join(static_dir,'css')
-    }
-    sass_cmd = "sass --trace -I {sass} {sass}/main.sass {css}/main.css".format(**sass_conf)
-    os.system(sass_cmd)
-    coffee_globs = ['*.coffee', '**/*.coffee', '**/**/*.coffee'] # 3 levels deep
-    coffee_conf = {
-        'src': path.join(static_dir, 'coffee'),
-        'out': path.join(static_dir, 'js')
-    }
-    coffee_cmd = "coffee -c -b --no-header -o {out} {src}".format(**coffee_conf)
-    from livereload import Server
-    server = Server(app.wsgi_app)
-    for coffee_glob in coffee_globs:
-        dir_glob = path.join(static_dir, 'coffee', coffee_glob)
-        server.watch(dir_glob, coffee_cmd)
-    for sass_glob in sass_globs:
-        dir_glob = path.join(static_dir, 'sass', sass_glob)
-        server.watch(dir_glob, sass_cmd)
-    server.serve(port=port)
-
 if __name__ == '__main__':
     manager.run()
 
