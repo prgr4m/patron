86d2d2b5b9405421cffa7268128a60fa7578b3fb
prgr4m <ping_me@johnboisselle.com>
2014-09-11 02:30:55 -0400

    reorganized code a little bit and removed addons directory since everything should be able to fit into one file

---
+++ lib/stencil/generators/addons.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+This file contains a list of functions to generate addons since it there isn't
+any state to be maintained other than within the StencilConfig object when
+dealing with a generated flask project.
+"""
+
+def generate_admin():
+    # read from config where the admin blueprint will be residing
+    # create files over
+    # hook into app factory (both the admin blueprint and auth)
+    # hook into manage.py the commands
+    pass
