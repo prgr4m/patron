26060a45a4f93d09f9b1b10ae1e5aed19e714794
prgr4m <ping_me@johnboisselle.com>
2014-09-14 02:02:27 -0400

    checking in work thus far... models are crazy

--- lib/stencil/generators/model.py
+++ lib/stencil/generators/model.py
@@ -55,13 +55,24 @@ class ModelGenerator(object):
         generate_templates(template_root, template_file)
 
     def _parse_fields(self, *fields):
-        # check each item to make sure that they are formatted, if not, drop it
-        # make sure its type is recognized to an extentent and to capitalize it
         field_stmt_def = "{indent}{name} = db.Column({field_type}){linesep}"
+        # for the time being its only name:field_type and only supporting basic
+        # types: integer, string(size), text, datetime, float, boolean
+        #
+        # eventually I'll add more (like length - if applicable) and parameters
+        # passed to the Column definition...
+        # also, I could just create an empty column for custom types or for
+        # defining relationships between models
         for field in fields:
             if ':' not in field:
+                # debating on just giving name and making empty column or for
+                # defining a relationship field... for now I just want to make
+                # this work and then I'll refine it...
                 continue
-            # check for type
+            # else:  # for later when I add the new features
+            attribs = field.split(':')
+
+            # look up type and inject to field_type
             field_def = field_stmt_def.format(indent=self.indent,
                                               name=None,
                                               field_type=None,
@@ -71,20 +82,43 @@ class ModelGenerator(object):
     @contextmanager
     def _model(self):
         model_def = "class {}(db.Model):".format(self.name)
+        table_def = "{}__tablename__ = '{}'".format(self.indent, self.name)
         id_def = "{}id = db.Column(db.Integer, primary_key=True)"\
             .format(self.indent)
         print(os.linesep)
-        print(model_def)
-        print(id_def)
+        print(model_def + os.linesep)
+        print(table_def + os.linesep)
+        print(id_def + os.linesep)
         yield
         print(os.linesep)
-        print("def __str__(self):")
-        print("{}pass".format(self.indent))
+        print("def __str__(self):" + os.linesep)
+        print("{}pass".format(self.indent) + os.linesep)
         print(os.linesep)
-        print("def __repr__(self):")
+        print("def __repr__(self):" + os.linesep)
         print("{}return \"<{}: Customize me!>\"".format(self.indent,
                                                         self.name))
         print(os.linesep)
+
+    def get_known_fields(self):
+        field_map = {
+            'smallint': 'db.SmallInteger',
+            'integer': 'db.Integer',
+            'bigint': 'db.BigInteger'
+            'string': 'db.String(50)',
+            'text': 'db.Text',
+            'date': 'db.Date',
+            'datetime': 'db.DateTime',
+            'time': 'db.Time()',
+            'enum': 'db.Enum()',
+            'float': 'db.Float()',
+            'numeric': 'db.Numeric()',
+            'bool': 'db.Boolean()',
+            'binary': 'db.LargeBinary()',
+            'pickle': 'db.PickleType()',
+            'unicode': 'db.Unicode()',
+            'unitext': 'db.UnicodeText()'
+        }
+        return field_map
 
     def _serialize(self):
         with open(self.output_target, 'a') as outfile:
