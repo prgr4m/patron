409aca646026ca143302350accdbe35d47db9772
prgr4m <ping_me@johnboisselle.com>
2014-09-23 07:27:02 -0400

    added dynamic page loading to public blueprint

--- lib/stencil/templates/project/public/views.py
+++ lib/stencil/templates/project/public/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import datetime as dt
 import os.path as path
+from flask import abort
 from flask.blueprints import Blueprint
 from flask.globals import current_app
 from flask.helpers import make_response
 from flask.templating import render_template
+from jinja2.exceptions import TemplateNotFound
 
 templates_dir = path.join(path.dirname(path.abspath(__file__)), 'templates')
 frontend = Blueprint('frontend', __name__, template_folder=templates_dir)
@@ -15,9 +17,13 @@ def processor():
     return dict()
 
 
-@frontend.route('/')
-def index():
-    return render_template('index.jade')
+@frontend.route('/', defaults={'page': 'index'})
+@frontend.route('/<page>')
+def show(page):
+    try:
+        return render_template("%s.jade")
+    except TemplateNotFound:
+        abort(404)
 
 
 @frontend.route('/sitemap.xml', methods=['GET'])
