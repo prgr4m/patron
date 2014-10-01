96251d68c981c98458dabd84f8ac9551ad119245
prgr4m <prgr4m@yahoo.com>
2014-10-01 03:24:54 -0400

    added docs dir and removed personal info so others can use this tool
    --HG--
    rename : patron/data/project/root/bowerrc => patron/data/project/root/bowerrc.txt

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. Patron documentation master file, created by
+   sphinx-quickstart on Wed Oct  1 03:09:30 2014.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Patron's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Patron is a generator modeled after the Padrino framework's generators but for
+the flask microframework.
+"""
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+setup(
+    name = "patron",
+    version = "0.2.0",
+    description = "Flask generators influenced by Padrino's generators",
+    long_description = __doc__,
+    author = "John Boisselle",
+    author_email = "prgr4m@yahoo.com",
+    url = "https://bitbucket.org/prgr4m/patron",
+    license = "MIT",
+    packages = find_packages(),
+    package_data = {
+        '': ['data/**/*.*']
+    },
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audince :: Flask Developers',
+        'Topic :: Software Development :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords = "flask generators scaffolding",
+    entry_points = {
+        'console_scripts': [
+            'patron=patron:main'
+        ]
+    }
+)
