be46523921945d6030bcad0c01269cc4b052e18e
prgr4m <prgr4m@yahoo.com>
2014-10-19 18:58:58 -0400

    removed old references to files that no longer exist
    --HG--
    branch : cookiecutter

--- patron/data/blueprint/forms.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask_wtf.form import Form
-from wtforms.fields import StringField
-from wtforms import validators as val
-
-
--- patron/data/blueprint/models.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-import datetime as dt
-from ..extensions import db
-
