9e5dd60e4f8c7c7954730d815eb81833d23c78bf
prgr4m <prgr4m@yahoo.com>
2014-11-08 15:31:21 -0500

    removed the java from python when generating the parser

--- patron/__init__.py
+++ patron/__init__.py
@@ -3,20 +3,15 @@
 # PYTHON_ARGCOMPLETE_OK
 from __future__ import print_function
 # import argcomplete
-from .parser import PatronParser
+from .parser import get_parser
 from .helpers import check_dependencies, setup_user_directory
 from .project import create_project
 
 __version__ = '0.2.3'
 cli_args = None
-
-#     project_type[args.subparser_name](**options).create()
-#     if args.subparser_name == 'model':
 #         # takes a name and *fields of name:type
 #         ModelGenerator(args.namespace, args.name).create(args.field)
 #         # takes a name and *fields of name:type
-#     elif args.subparser_name == 'task':
-#         TaskGenerator(args.name, args.description).create()
 
 
 def init_parser():
@@ -47,6 +42,10 @@ def model_parser():
     print(cli_args)
 
 
+def package_parser():
+    print(cli_args)
+
+
 def addon_parser():
     print(cli_args)
 
@@ -56,34 +55,17 @@ def task_parser():
 
 
 def main():
-    # addon_parser = subparser.add_parser('addon', help=addon_help)
-    # name_group = [project_parser, models_parser, blueprint_parser,
-    #               task_parser, static_parser]  # add back forms_parser later
-    # for p in name_group:
-    #     p.add_argument('name', help=name_help[p])
-    # addon_parser.add_argument('name', choices=Patron.get_addons(),
-    #                           help=addon_name_help)
-    # # field_group = [models_parser, forms_parser]
-    # field_group = [models_parser]
-    # model_field_ex = "Ex: 'username:string'"
-    # model_field_help = "field_name:sqlalchemy_type {} {}"\
-    #     .format(Patron.get_field_types(), model_field_ex)
-    # field_help = {
-    #     models_parser: model_field_help,
-    #     # forms_parser: 'field_name:wtforms_type'
-    # }
     # for p in field_group:
     #     p.add_argument('field', nargs='*', help=field_help[p])
-    # task_description_help = "description of the task"
-    # task_parser.add_argument('description', help=task_description_help)
     # argcomplete.autocomplete(parser)
     global cli_args
-    parser = PatronParser.get_parser()
+    parser = get_parser()
     subparser_processor = {
         'init': init_parser,
         'project': project_parser,
         'blueprint': blueprint_parser,
         'model': model_parser,
+        'pkg': package_parser,
         'addon': addon_parser,
         'task': task_parser
     }
--- patron/parser.py
+++ patron/parser.py
@@ -2,81 +2,74 @@
 import argparse
 from .config import PatronConfig
 
-PARSER_DESC = "Patron - a generator for flask projects inspired by padrino"
+parser, subparser = None, None
+parser_desc = "Patron - a generator for flask projects inspired by padrino"
 
 
-class PatronParser(object):
-    def __init__(self, parser_type='main'):
-        if parser_type not in ['main', 'project']:
-            raise StandardError("PatronParser:Unknown parser type")
-        self.parser = argparse.ArgumentParser(description=PARSER_DESC)
-        self.parser_type = parser_type
-        self.subparser = self.parser.add_subparsers(dest='subparser_name')
+def get_parser():
+    global parser, subparser, parser_desc
+    parser = argparse.ArgumentParser(description=parser_desc)
+    subparser = parser.add_subparsers(dest='subparser_name')
+    if PatronConfig.is_present():
+        project_parser()
+    else:
+        main_parser()
+    return parser
 
-    @staticmethod
-    def get_parser():
-        if PatronConfig.is_present():
-            parser = PatronParser(parser_type='project').create()
-        else:
-            parser = PatronParser().create()
-        return parser
 
-    def create(self):
-        getattr(self, "_{}_parser".format(self.parser_type))()
-        return self.parser
+def main_parser():
+    init_help = "actions to be performed with patron itself"
+    init_parser = subparser.add_parser('init', help=init_help)
+    init_choices = ('check', 'user')
+    choices_help = """
+    'check' external dependencies or create the 'user' directory which
+    contains the templates for the user to override to their liking."""
+    init_parser.add_argument('action', choices=init_choices, help=choices_help)
+    project_help = "create a flask project"
+    name_help = "name of the flask project"
+    project_parser = subparser.add_parser('project', help=project_help)
+    project_parser.add_argument('name', help=name_help)
+    dir_help = "rename target directory keeping project internals"
+    project_parser.add_argument('-d', '--directory', help=dir_help)
 
-    def _main_parser(self):
-        init_help = "actions to be performed with patron itself"
-        init_parser = self.subparser.add_parser('init', help=init_help)
-        init_choices = ('check', 'user')
-        choices_help = """
-        'check' external dependencies or create the 'user' directory which
-        contains the templates for the user to override to their liking."""
-        init_parser.add_argument('action', choices=init_choices,
-                                 help=choices_help)
-        project_help = "create a flask project"
-        name_help = "name of the flask project"
-        project_parser = self.subparser.add_parser('project', help=project_help)
-        project_parser.add_argument('name', help=name_help)
 
-        dir_help = "rename target directory keeping project internals"
-        project_parser.add_argument('-d', '--directory', help=dir_help)
+def project_parser():
+    add_blueprint()
+    add_model()
+    add_addons()
+    add_task()
 
-    def _project_parser(self):
-        self._add_blueprint()
-        self._add_model()
-        self._add_addons()
-        self._add_task()
 
-    def _add_blueprint(self):
-        blueprint_help = "create a blueprint"
-        blueprint_name_help = "name of blueprint"
-        blueprint_route_help = "route_name:methods:variable-type"
-        blueprint_parser = self.subparser.add_parser('blueprint',
-                                                     help=blueprint_help)
-        blueprint_parser.add_argument('name', help=blueprint_name_help)
-        blueprint_parser.add_argument('route', nargs='*',
-                                      help=blueprint_route_help)
+def add_blueprint():
+    blueprint_help = "create a blueprint"
+    blueprint_name_help = "name of blueprint"
+    blueprint_route_help = "route_name:methods:variable-type"
+    blueprint_parser = subparser.add_parser('blueprint', help=blueprint_help)
+    blueprint_parser.add_argument('name', help=blueprint_name_help)
+    blueprint_parser.add_argument('route', nargs='*', help=blueprint_route_help)
 
-    def _add_model(self):
-        model_help = "generate a model for SQLAlchemy"
-        model_blueprint_help = "name of blueprint"
-        model_name_help = "name of model"
-        model_field_help = "name:sqlalchemy_type-value:column_attr-value"
-        model_parser = self.subparser.add_parser('model', help=model_help)
-        model_parser.add_argument('blueprint', help=model_blueprint_help)
-        model_parser.add_argument('name', help=model_name_help)
-        model_parser.add_argument('field', help=model_field_help)
 
-    def _add_addons(self):
-        # check addons
-        # don't add the ones that already exist
-        pass
+def add_model():
+    model_help = "generate a model for SQLAlchemy"
+    model_blueprint_help = "name of blueprint"
+    model_name_help = "name of model"
+    model_field_help = "name:sqlalchemy_type-value:column_attr-value"
+    model_parser = subparser.add_parser('model', help=model_help)
+    model_parser.add_argument('blueprint', help=model_blueprint_help)
+    model_parser.add_argument('name', help=model_name_help)
+    model_parser.add_argument('field', help=model_field_help)
 
-    def _add_task(self):
-        task_help = "create task to be used with fabric"
-        task_name_help = "name of the task"
-        task_desc_help = "description of the task"
-        task_parser = self.subparser.add_parser('task', help=task_help)
-        task_parser.add_argument('name', help=task_name_help)
-        task_parser.add_argument('description', help=task_desc_help)
+
+def add_addons():
+    # check addons
+    # don't add the ones that already exist
+    pass
+
+
+def add_task():
+    task_help = "create task to be used with fabric"
+    task_name_help = "name of the task"
+    task_desc_help = "description of the task"
+    task_parser = subparser.add_parser('task', help=task_help)
+    task_parser.add_argument('name', help=task_name_help)
+    task_parser.add_argument('description', help=task_desc_help)
