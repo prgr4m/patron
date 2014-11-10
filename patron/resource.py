e5db035b0644f7163180d3cd4d7f50f264bcb16a
prgr4m <prgr4m@yahoo.com>
2014-11-10 05:43:26 -0500

    checking in work before seriously passing out

--- patron/resource.py
+++ patron/resource.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 from os import path, linesep
 import sys
+import shutil
 from cookiecutter.generate import generate_files
 from . import config
 from .helpers import is_name_valid, get_scaffold, create_context, get_stream
@@ -41,7 +42,7 @@ def create_blueprint(name, routes=None, templates=True):
                                                 templates)
             # print to route_content
             # if templates:
-            #     create_route_template(route_name)
+            #     create_route_template(name, route_name)
             # route_stmt = "{}{}".format(linesep, )
         print(route_content.getvalue())
         # with open(view_filename, 'at') as view_file:
@@ -52,30 +53,61 @@ def create_blueprint(name, routes=None, templates=True):
 
 
 def build_route_definition(blueprint_name, route_name, methods, variables):
-    # @bp_name.route('/route_name', methods=['GET','POST'])
+    http_verbs = ('GET', 'POST', 'PUT', 'DELETE')
+    variable_types = ('int', 'float', 'path')
     route_stmt = "@{bp_name}.route('/{route_name}{variables}'{methods})"
-    route_data = dict(bp_name=blueprint_name)
-    # validate each part
+    variable_prefix = "/{variables}"
+    method_prefix = ", methods=[{methods}]"
+    route_data = dict(bp_name=blueprint_name, route_name=route_name)
+    methods = methods.upper()
+    if '-' in methods:
+        methods = methods.split('-')
+        quote_methods = ",".join(['%s' % m for m in methods if m in http_verbs])
+        route_data['methods'] = method_prefix.format(methods=quote_methods)
+    elif methods in http_verbs and methods != 'GET':
+        quoted_method = "'{}'".format(methods)
+        route_data['methods'] = method_prefix.format(methods=quoted_method)
+    else:  # just a 'GET' method
+        route_data['methods'] = ''
+    if variables:
+        for variable in variables:
+            if '-' in variable:
+                pass  # check for type
+            else:
+                pass
+    else:
+        route_data['variables'] = ''
 
 
-def build_route_handler(route_parts, variables, templates):
+def build_route_handler(route_name, variables, templates):
     indent = " " * 4
-    # def route_name():
-    #     pass|return render_template('{route_name}.jade')
+    func_def_stmt = "def {route_name}({variables}):"
+    func_data = dict(route_name=route_name)
+    if variables:
+        for variable in variables:
+            if '-' in variable:
+                pass
+            else:
+                pass
+    else:
+        func_data['variables'] = ''
+    func_def = func_def_stmt.format(**func_data)
     if templates:
         body_def_stmt = "{indent}return render_template('{route_nm}.jade')"
         body_def = body_def_stmt.format(indent=indent, route_nm=route_name)
     else:
         body_def_stmt = "{indent}pass"
         body_def = body_def_stmt.format(indent=indent)
-    return [route_def, body_def]
+    return [func_def, body_def]
 
 
-def create_route_template(route_name):
-    # get scaffold directory
-    # fetch route_template
-    # copy over to templates directory as 'route_name'.jade
-    pass
+def create_route_template(blueprint_name, route_name):
+    scaffold = get_scaffold('blueprint')
+    src_file = path.join(scaffold, 'route_template.jade')
+    project_name = config.get_project_name()
+    target_file = path.join(project_name, blueprint_name, 'templates',
+                            "{}.jade".format(route_name))
+    shutil.copyfile(src_file, target_file)
 
 
 def create_package(name, options):
