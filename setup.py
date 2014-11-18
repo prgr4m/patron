fc782ca6132b3763a6294a0f01fb288a3fdfe28d
prgr4m <prgr4m@yahoo.com>
2014-11-17 22:59:07 -0500

    updated task file. removed os independent because I haven't tested on windows

--- setup.py
+++ setup.py
@@ -28,8 +28,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Operating System :: OS Independent'
+        'Programming Language :: Python :: 3.4'
     ],
     install_requires=['cookiecutter>=0.7.1'],
     keywords="flask generators scaffolding cli",
